710c1c6eed582d24929fc27fc0d508c19d60804e
Soroush Zare <ihaveint@gmail.com>
2024-07-23 15:52:06 -0700

    TODO: make it run again

--- lib/mapreduce.ex
+++ lib/mapreduce.ex
@@ -17,20 +17,21 @@ defmodule Mapreduce do
   end
 
   def from_file(file_path \\ "sample.txt") do
-    binary = File.read!(file_path)
-    # IO.inspect(binary)
-
-    binary
+    stream = File.stream!(file_path)
+    stream
   end
 
-  def solve(mapper_func, reducer_func, file_path_collection) do
-    files_dump = Enum.map(file_path_collection, fn file_path ->
-      # take the file path. convert it to the data the file has
-      from_file(file_path)
-    end)
+  def solve(mapper_func, reducer_func, data \\ from_file ) do
+    # files_dump =
+    #   Stream.map(file_path_collection, fn file_path ->
+    #     # take the file path. convert it to the data the file has
+    #     from_file(file_path)
+    #   end)
 
-    collection_of_strings = Enum.map(files_dump, fn str -> String.split(str, " ") end)
-
+    collection_of_strings =
+      Stream.map(files_dump, fn str -> String.split(str, " ") end)
+      |> Enum.map(fn strings -> remove_special_chars(strings) end)
+      |> Enum.map(fn strings -> to_lowercase(strings) end)
 
     Enum.map(collection_of_strings, mapper_func)
   end
@@ -38,36 +39,49 @@ defmodule Mapreduce do
   def sample_mapper(string_collection) do
     Enum.map(string_collection, fn str -> {str, 1} end)
   end
-end
 
-list = ["apple", "banana", "orange", "mango", "strawberries", "blueberries", "strawberries", "apple", "apple", "apple"]
-count = length(list)
+  list = [
+    "apple",
+    "banana",
+    "orange",
+    "mango",
+    "strawberries",
+    "blueberries",
+    "strawberries",
+    "apple",
+    "apple",
+    "apple"
+  ]
 
-def setToMax(file_path) do
-  if count >= 5000 
-    count = 5000
-end
+  count = length(list)
 
-def split(file_path) do
-  chunks_of_5 = Enum.chunk_every(list, 5)
-  Enum.map(chunks, fn chunk ->
-      length = div(length(chunk), 5)
-      Enum.chunk_every(chunk, length)
+  # def setToMax(file_path) do
+  #   if(count >= 5000)
+  #   count = 5000
+  # end
+
+  # def split(file_path) do
+  #   chunks_of_5 = Enum.chunk_every(list, 5)
+
+  #   Enum.map(chunks, fn chunk ->
+  #     length = div(length(chunk), 5)
+  #     Enum.chunk_every(chunk, length)
+  #   end)
+  # end
+
+  def remove_special_chars(strings) do
+    strings
+    |> Enum.map(fn string ->
+      string
+      |> String.replace(~r/[.!?,]/, "") # needs to be fixed
     end)
-end
+  end
 
-
-
-def remove_special_chars(file_path) do
-  file_path
-  File.read!()
-  String.replace(".|!|?|,", "")
-  File.write!(file_path)
-end
-
-def to_lowercase(file_path) do
-  file_path
-  File.read!()
-  String.downcase()
-  File.write!(file_path)
+  def to_lowercase(strings) do
+    strings
+    |> Enum.map(fn string ->
+      string
+      |> String.downcase()
+    end)
+  end
 end
